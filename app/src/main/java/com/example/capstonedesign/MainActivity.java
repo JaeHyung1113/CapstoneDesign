814470d6e0d98d40dbcbbc6c24822c7f375fe966
woochanghan <hwc1156@naver.com>
2022-04-28 09:39:43 +0900

    NDK 1.1

--- app/src/main/java/com/example/capstonedesign/MainActivity.java
+++ app/src/main/java/com/example/capstonedesign/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.capstonedesign;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
-import android.util.Log;
 
-import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView text;
@@ -17,13 +15,14 @@ public class MainActivity extends AppCompatActivity {
     public native String print_ndk(String text);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         text = (TextView) findViewById(R.id.text);
+
         String print = print_ndk("hello_ndk");
-        Log.d("test",print);
+
         text.setText(print);
     }
 }
