71ebf12a3db294086e0dfe1c5b51a9d28e17d2fe
woochang <hwc1156@gmail.com>
2022-06-10 00:16:03 +0900

    기본 카메라 호출 및 찍은 이미지 가져오기

--- app/src/main/java/com/example/capstonedesign/MainActivity.java
+++ app/src/main/java/com/example/capstonedesign/MainActivity.java
@@ -1,27 +1,192 @@
 package com.example.capstonedesign;
 
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private MultiplePermission permission;
+
+    static File currentPhotoFile;
+    static Uri currentPhotoUri;
+    static String currentPhotoPath;
+    static String currentPhotoFileName;
+
     Button check;
+    Button capture;
+    Button test;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        check = (Button) findViewById(R.id.btn_check);
-        check.setOnClickListener(new View.OnClickListener() {
+
+        permissionCheck();
+
+        test = (Button) findViewById(R.id.btn_test);
+        test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
                 startActivity(intent);
             }
         });
+        capture = (Button) findViewById(R.id.btn_capture);
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    capturePhoto();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void permissionCheck() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            permission = new MultiplePermission(this, this);
+        }
+        if(!permission.checkPermission()) {
+            permission.requestPermission();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (!permission.permissionResultRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            permission.requestPermission();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void capturePhoto() throws IOException {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File imageFile = createImageFile();
+            if(imageFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, currentPhotoUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // 이미지 파일 이름
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        // 외부저장소
+        File storageDir = getExternalFilesDir("CapstoneDesign/");
+
+        // 파일 생성
+        File newFile = File.createTempFile(imageFileName, ".jpg", storageDir );
+
+        currentPhotoFile = newFile;
+        currentPhotoFileName = newFile.getName();
+        currentPhotoPath = newFile.getAbsolutePath(); // 파일의 절대경로
+
+        try {
+            currentPhotoUri = FileProvider.getUriForFile(this,
+                    getApplicationContext().getPackageName() + ".fileprovider",
+                    newFile);
+        } catch (Exception ex) {
+            Log.d("FileProvider", ex.getMessage());
+            ex.printStackTrace();
+            throw ex;
+        }
+        return newFile;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            if(currentPhotoFile != null) {
+                imageView.setImageURI(currentPhotoUri);
+                // 갤러리에 이미지 파일 생성
+              //  galleryAddPic(currentPhotoUri, currentPhotoFileName);
+            }
+        }
+    }
+    private Uri galleryAddPic(Uri srcImageFileUri ,String srcImageFileName) {
+        ContentValues contentValues = new ContentValues();
+        ContentResolver contentResolver = getApplicationContext().getContentResolver();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, srcImageFileName);
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/MyImages"); // 두개의 경로[DCIM/ , Pictures/]만 가능함 , 생략시 Pictures/ 에 생성됨
+        contentValues.put(MediaStore.Images.Media.IS_PENDING, 1); //다른앱이 파일에 접근하지 못하도록 함(Android 10 이상)
+        Uri newImageFileUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,contentValues);
+
+        try {
+            AssetFileDescriptor afdInput = contentResolver.openAssetFileDescriptor(srcImageFileUri, "r");
+            AssetFileDescriptor afdOutput = contentResolver.openAssetFileDescriptor(newImageFileUri, "w");
+            FileInputStream fis = afdInput.createInputStream();
+            FileOutputStream fos = afdOutput.createOutputStream();
+
+            byte[] readByteBuf = new byte[1024];
+            while(true){
+                int readLen = fis.read(readByteBuf);
+                if (readLen <= 0) {
+                    break;
+                }
+                fos.write(readByteBuf,0,readLen);
+            }
+
+            fos.flush();
+            fos.close();
+            afdOutput.close();
+
+            fis.close();
+            afdInput.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        contentValues.clear();
+        contentValues.put(MediaStore.Images.Media.IS_PENDING, 0); //다른앱이 파일에 접근할수 있도록 함
+        contentResolver.update(newImageFileUri, contentValues, null, null);
+        return newImageFileUri;
     }
 }
